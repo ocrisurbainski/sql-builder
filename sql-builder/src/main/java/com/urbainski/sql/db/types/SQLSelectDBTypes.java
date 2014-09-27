e3dae7730a401c63d4f104085564169ac2f10de2
CristianUrbainski <cristianurbainskips@gmail.com>
2014-09-27 19:10:06 -0300

    adicionando enum para as clausulas sql

---
+++ sql-builder/src/main/java/com/urbainski/sql/db/types/SQLSelectDBTypes.java
@@ -0,0 +1,65 @@
+package com.urbainski.sql.db.types;
+
+/**
+ * Enum para os tipos comuns de clausulas dos selects.
+ * 
+ * @author Cristian Urbainski <cristian.urbainski@consisanet.com>
+ * @since 27/09/2014
+ *
+ */
+public enum SQLSelectDBTypes {
+	
+	/**
+	 * Clausula 'as' do SQL.
+	 */
+	AS("as"),
+	
+	/**
+	 * Clausula 'select' do SQL.
+	 */
+	SELECT("select"),
+	
+	/**
+	 * Clausula 'distinct' do SQL.
+	 */
+	DISTINCT("distinct"),
+	
+	/**
+	 * Clausula 'from' do SQL.
+	 */
+	FROM("from"),
+	
+	/**
+	 * Clausula 'where' do SQL.
+	 */
+	WHERE("where"),
+	
+	/**
+	 * Clausula 'group by' do SQL.
+	 */
+	GROUP_BY("group by"), 
+	
+	/**
+	 * Clausula 'order by' do SQL.
+	 */
+	ORDER_BY("order by");
+
+	/**
+	 * Nome da clausula de sql.
+	 */
+	private String name;
+	
+	/**
+	 * Construtor privado do enum.
+	 * 
+	 * @param name - Nome da clausula de sql.
+	 */
+	private SQLSelectDBTypes(String name) {
+		this.name = name;
+	}
+	
+	public String getSQLSelectType() {
+		return name;
+	}
+	
+}
